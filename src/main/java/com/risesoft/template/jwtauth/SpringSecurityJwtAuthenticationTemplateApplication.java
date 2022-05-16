9195c294d863a5fb1d592b5f90664e9b045ef0bd
rst0070 <52444137+rst0070@users.noreply.github.com>
2022-05-16 16:03:28 +0900

    create project

---
+++ src/main/java/com/risesoft/template/jwtauth/SpringSecurityJwtAuthenticationTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.risesoft.template.jwtauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtAuthenticationTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityJwtAuthenticationTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/risesoft/template/jwtauth/SpringSecurityJwtAuthenticationTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.risesoft.template.jwtauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtAuthenticationTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
